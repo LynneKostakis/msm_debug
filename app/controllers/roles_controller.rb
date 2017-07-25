e360bc36197404bb1b62c0228aef446e2d1e59c7
Lynne Kostakis <lkostaki@chicagobooth.edu>
2017-07-25 22:51:36 +0000

    fixed

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -24,7 +24,7 @@ class RolesController < ApplicationController
   def edit_form
     @role = Role.find(params[:id])
     
-    render("rolwe/edit_form.html.erb")
+    render("actor/edit_form.html.erb")
   end
 
   def update_row
